58429877e9dc58f607a0eea26e6dd63c632651e3
hoshina <k-hoshina@bns-g.com>
2016-08-03 17:28:09 +0900

    app/

---
+++ app/app_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	ts := httptest.NewServer(nil)
+	defer ts.Close()
+
+	// リクエストの送信先はテストサーバのURLへ。
+	r, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatalf("Error by http.Get(). %v", err)
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		t.Fatalf("Error by ioutil.ReadAll(). %v", err)
+	}
+
+	if "Hello World!" != string(data) {
+		t.Fatalf("Data Error. %v", string(data))
+	}
+}

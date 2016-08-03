cdec2e6cd6095673df0925bd341611cc4add0b36
hoshina <k-hoshina@bns-g.com>
2016-08-03 17:22:33 +0900

    app

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", helloWorld)
+}
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
